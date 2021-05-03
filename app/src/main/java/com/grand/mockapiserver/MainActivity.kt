c835fcd0f461ff7c41daf8725c701ac5c9deb99f
jiang.junmin <jiangjm8888@gmail.com>
2021-05-03 10:43:03 +0800

    添加对应权限，否则服务无法启动

--- app/src/main/java/com/grand/mockapiserver/MainActivity.kt
+++ app/src/main/java/com/grand/mockapiserver/MainActivity.kt
@@ -3,6 +3,7 @@ package com.grand.mockapiserver
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.grand.mockapiserver.core.MockWebServerService
 
 class MainActivity : AppCompatActivity() {
@@ -10,8 +11,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            Log.i("jiang","start foreground service")
             startForegroundService(Intent(this, MockWebServerService::class.java))
         } else {
+            Log.i("jiang","start service")
             startService(Intent(this, MockWebServerService::class.java))
         }
     }
